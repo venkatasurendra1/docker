db16e2ec6b41d0250cda6de64eaecaf40e7ad1f2
venkatasurendra1 <37298001+venkatasurendra1@users.noreply.github.com>
2018-08-05 01:18:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+sudo cp WebApp.war /opt
 sudo cp dockerfile /opt
 sudo cd /opt
-sudo docker build -t tomcat .
+sudo docker build -t tomcat /opt
 sudo docker tag tomcat venkatasurendra1/build:latest
 sudo docker push venkatasurendra1/build:latest
